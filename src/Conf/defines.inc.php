4e1a24e5a4accc7000815066eea4ccc1ef9f77c7
BadPixxel <eshop.bpaquier@gmail.com>
2019-05-09 03:57:22 +0200

    WIP

---
+++ src/Conf/defines.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of BadPixxel Dolibarr Helper Library.
+ *
+ * Copyright (C) 2015-2019 BadPixxel  <www.badpixxel.com>
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+//====================================================================//
+// Library Version
+define('DOL_HELPER_VERSION', "0.0.0");
+
+//====================================================================//
+// Library Configuration
+define('DOL_HELPER_NAME', "BadPixxel Dolibarr Helper Library");
+define('DOL_HELPER_CODE', "BP Helpers");
+define('DOL_HELPER_AUTHOR', "BadPixxel - B. Paquier");
+define('DOL_HELPER_LOG_PREFIX', "BadPixxel Helper: ");
+
+if (!defined('DOL_HELPER_DEBUG')) {
+    define('DOL_HELPER_DEBUG', true);
+}
